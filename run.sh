7500975ca3face6a290cc40cbcfb933d09a8a2d8
Mike Cochran <mcochran@linux.com>
2015-03-28 15:22:16 -0500

    Separate slides path. orighost env

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ export GOROOT=~/.go
 export GOPATH=~/.gopath
 export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 go get golang.org/x/tools/cmd/present
-present -base .html/ -http "0.0.0.0:$PORT" -orighost "slides.vongrippen.com"
+cd slides
+present -base .html/ -http "0.0.0.0:$PORT" -orighost "$ORIGHOST"
